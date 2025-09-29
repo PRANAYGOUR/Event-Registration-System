4d864729ae33b1481d4d00b1527e5de09bde03c2
Pranay Gour <76206607+PRANAYGOUR@users.noreply.github.com>
2025-09-29 10:02:03 +0530

    Update db.js

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-module.exports = function connectDB() {
-  const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/eventdb';
-  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => {
-      console.error('MongoDB connection error:', err);
-      process.exit(1);
-    });
+module.exports = async function connectDB() {
+  const uri = process.env.MONGO_URI || "mongodb://localhost:27017/eventdb";
+  try {
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  }
 };
